2b5262511c93b9b9114e7520d392c9f42abddfb1
initial commit <geraldoramos@GRamos-Macbook.domain>
2017-04-09 01:29:16 -0600

    removed sh from init commands and updated changeling

--- modules/dropbox/on_movie_end.sh
+++ modules/dropbox/on_movie_end.sh
@@ -1,4 +1,4 @@
 
 #Define on_movie_end routine
 echo "on_movie_end routine for Dropbox"
-sudo sh ./modules/dropbox/binaries/dropbox_uploader.sh upload %f /detections/ && rm -rf %f
+sudo ./modules/dropbox/binaries/dropbox_uploader.sh upload %f /detections/ && rm -rf %f
