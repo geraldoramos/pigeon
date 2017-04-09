25cc508bbec7d0a4bc041cf30a4ac082d405c604
initial commit <geraldoramos@GRamos-Macbook.domain>
2017-04-09 14:23:23 -0600

    made modules installation optional

--- pigeon_modules_init.sh
+++ pigeon_modules_init.sh
@@ -6,6 +6,19 @@
 echo "Starting Modules script"
 
 for d in modules/*/ ; do
+
+  if [[ $1 == 'install' ]]
+    then
+    echo -n "Do you want install $d ? [y/N]"
+    read install
+      if [[ $install == "y" ]] || [[ $install == "Y" ]] || [[ $intall == "Yes" ]] || [[ $install == "yes" ]]
+        then
+        chmod +x '/home/pi/pigeon/'$d$1'.sh'
+        sudo '/home/pi/pigeon/'$d$1'.sh' $2
+        fi
+  else
   chmod +x '/home/pi/pigeon/'$d$1'.sh'
   sudo '/home/pi/pigeon/'$d$1'.sh' $2
+  fi
+
 done
