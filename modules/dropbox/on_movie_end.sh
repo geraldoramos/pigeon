880cae61606522a40a311dba5c4854caaeb0ccc4
initial commit <geraldoramos@GRamos-Macbook.domain>
2017-04-15 20:42:53 -0600

    removed echo

--- modules/dropbox/on_movie_end.sh
+++ modules/dropbox/on_movie_end.sh
@@ -3,5 +3,4 @@
 #Define on_movie_end routine for this module. Path to file is available through variable $1 and name of the camera through variable $2
 
 echo "on_movie_end routine for Dropbox"
-echo $1 $2
 sudo /home/pi/pigeon/modules/dropbox/binaries/dropbox_uploader.sh -f /home/pi/pigeon/modules/dropbox/binaries/dropbox.conf upload $1 /$2/ && rm -rf $1
