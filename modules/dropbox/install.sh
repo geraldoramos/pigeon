2b31f492ae01dba7725bc0f81165cb118336f184
initial commit <geraldoramos@GRamos-Macbook.domain>
2017-04-15 20:46:17 -0600

    remove dropbox conf before install

--- modules/dropbox/install.sh
+++ modules/dropbox/install.sh
@@ -2,4 +2,5 @@
 
 # Define the installation routine for this module
 echo "Installing Dropbox"
+sudo rm -rf /home/pi/pigeon/modules/dropbox/binaries/dropbox.conf
 sudo ./modules/dropbox/binaries/dropbox_uploader.sh -f /home/pi/pigeon/modules/dropbox/binaries/dropbox.conf
