2810c7dfff6a3a92e9481b150856b1ffdbfeb229
initial commit <geraldoramos@GRamos-Macbook.domain>
2017-04-15 19:48:00 -0600

    copy changes

--- pigeon_modules_init.sh
+++ pigeon_modules_init.sh
@@ -3,7 +3,7 @@
 # This will run module-specific routines
 # Receives the type of argument to run (ex: install, on_movie_end or on_picture_save).
 
-echo "Starting Modules script"
+echo "Starting Modules Script"
 
 camera_name='pigeon'
 
